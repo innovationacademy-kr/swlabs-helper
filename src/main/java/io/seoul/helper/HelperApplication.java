6030c4fff35a7142b3e7d9f9795ff35f410b4f9c
platanus <platanus@canxan.com>
2021-06-18 16:15:20 +0900

    init

---
+++ src/main/java/io/seoul/helper/HelperApplication.java
@@ -0,0 +1,13 @@
+package io.seoul.helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelperApplication.class, args);
+	}
+
+}
