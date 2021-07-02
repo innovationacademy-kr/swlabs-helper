65f18658db30533e2fe28fa77d11c0e4f6ab3ec9
YoungHo Choi <0505zxc@gmail.com>
2021-07-03 07:04:21 +0900

    추가: create team 페이지 구현 /
    하드 코딩으로 pagination 임시값 설정 - 협의 후 수정 필요
    세부사항 버튼 클릭시 page 이동하도록 설정

--- src/main/java/io/seoul/helper/controller/PageController.java
+++ src/main/java/io/seoul/helper/controller/PageController.java
@@ -2,12 +2,22 @@ package io.seoul.helper.controller;
 
 import io.seoul.helper.config.auth.LoginUser;
 import io.seoul.helper.config.auth.dto.SessionUser;
+import io.seoul.helper.controller.team.dto.TeamResponseDto;
+import io.seoul.helper.domain.team.Team;
+import io.seoul.helper.domain.team.TeamStatus;
+import io.seoul.helper.repository.team.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageController {
+    @Autowired
+    private TeamRepository teamRepo;
 
     @GetMapping(value = "/")
     public String home(Model model, @LoginUser SessionUser user) {
@@ -29,7 +39,17 @@ public class PageController {
     }
 
     @GetMapping(value = "/create_team")
-    public String createTeam() {
+    public String createTeam(Model model,
+                             @RequestParam(value = "page", required = false, defaultValue = "0") int page) {
+        TeamStatus status = TeamStatus.WAITING;
+
+        Page<Team> teamPage = teamRepo.findAllByStatus(status, PageRequest.of(page, 2));
+        Page<TeamResponseDto> teams = teamPage.map(team -> new TeamResponseDto(team));
+
+        model.addAttribute("pages", teams);
+        model.addAttribute("maxPage", 3);
+        model.addAttribute("teams", teams);
+
         return "create_team";
     }
 }
