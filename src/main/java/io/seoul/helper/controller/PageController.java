b91653d75067163202a3fc042440ce2788c84e34
platanus <platanus@canxan.com>
2021-07-03 21:03:02 +0900

    추가: #84 팀 목록 페이지 구현 (DTO 생략)

--- src/main/java/io/seoul/helper/controller/PageController.java
+++ src/main/java/io/seoul/helper/controller/PageController.java
@@ -2,12 +2,20 @@ package io.seoul.helper.controller;
 
 import io.seoul.helper.config.auth.LoginUser;
 import io.seoul.helper.config.auth.dto.SessionUser;
+import io.seoul.helper.controller.team.dto.TeamListRequestDto;
+import io.seoul.helper.controller.team.dto.TeamResponseDto;
+import io.seoul.helper.service.TeamService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+
+    @Autowired
+    private TeamService teamService;
 
     @GetMapping(value = "/")
     public String home(Model model, @LoginUser SessionUser user) {
@@ -19,7 +27,11 @@ public class PageController {
     }
 
     @GetMapping(value = "/list_team")
-    public String teamList() {
+    public String teamList(Model model, @LoginUser SessionUser user) {
+        TeamListRequestDto dto = new TeamListRequestDto();
+        List<TeamResponseDto> teams = teamService.findTeams(dto);
+        model.addAttribute("teams", teams);
+        model.addAttribute("user", user);
         return "list_team";
     }
 
@@ -32,4 +44,7 @@ public class PageController {
     public String createTeam() {
         return "create_team";
     }
+
+
+
 }
