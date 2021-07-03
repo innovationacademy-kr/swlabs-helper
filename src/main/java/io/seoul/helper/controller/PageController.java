77cf020d5b54d6f633486298825f528d2ec39560
YoungHo Choi <0505zxc@gmail.com>
2021-07-03 21:10:35 +0900

    수정: nav 태그의 pagination 수정
    참고 코드: https://www.baeldung.com/spring-thymeleaf-pagination
    추가: active 버튼 활성화

--- src/main/java/io/seoul/helper/controller/PageController.java
+++ src/main/java/io/seoul/helper/controller/PageController.java
@@ -14,6 +14,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @Controller
 public class PageController {
     @Autowired
@@ -40,15 +44,22 @@ public class PageController {
 
     @GetMapping(value = "/create_team")
     public String createTeam(Model model,
-                             @RequestParam(value = "page", required = false, defaultValue = "0") int page) {
-        TeamStatus status = TeamStatus.WAITING;
+                             @RequestParam(value = "page", required = false, defaultValue = "1") int page,
+                             @RequestParam(value = "size", required = false, defaultValue = "5") int size) {
 
-        Page<Team> teamPage = teamRepo.findAllByStatus(status, PageRequest.of(page, 2));
+        TeamStatus status = TeamStatus.WAITING;
+        Page<Team> teamPage = teamRepo.findAllByStatus(status, PageRequest.of(page - 1, size));
         Page<TeamResponseDto> teams = teamPage.map(team -> new TeamResponseDto(team));
 
-        model.addAttribute("pages", teams);
-        model.addAttribute("maxPage", 3);
         model.addAttribute("teams", teams);
+
+        int totalPages = teams.getTotalPages();
+        if (totalPages > 0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
 
         return "create_team";
     }
