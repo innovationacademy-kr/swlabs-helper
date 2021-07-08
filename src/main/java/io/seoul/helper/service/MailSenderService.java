f3cb91293d7049781af90675c5ab0f41645b6b9d
HyunJin Won <ohj8447@gmail.com>
2021-07-08 23:55:09 +0900

    ADD: #102 mail service 송신 기능 추가 (#151)
    * ADD: #102 mail service 송신 기능 추가
    
    ADD: 메일 관련 service 추가, env 등록 필요
    EDIT: 메일 비동기 방식으로 수정
    EDIT: mentor 메칭 시 알림 메일 추가
    EDIT: modal 기능 오류 수정(id 오타 수정)
    
    * DELETE: #102 remove unused field

---
+++ src/main/java/io/seoul/helper/service/MailSenderService.java
@@ -0,0 +1,33 @@
+package io.seoul.helper.service;
+
+import io.seoul.helper.domain.team.Team;
+import io.seoul.helper.domain.user.User;
+import lombok.AllArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@EnableAsync
+public class MailSenderService {
+    private JavaMailSender mailSender;
+
+    @Async
+    public void sendMail(User user, String title, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(user.getEmail());
+        message.setSubject(title);
+        message.setText(content);
+
+        mailSender.send(message);
+    }
+
+    @Async
+    public void sendMatchMail(User user, Team team) {
+        sendMail(user, "Your Team Matched!", "Team\n" +
+                "\'#" + team.getId() + " : " + team.getProject().getName() + "\' is Matched!");
+    }
+}
