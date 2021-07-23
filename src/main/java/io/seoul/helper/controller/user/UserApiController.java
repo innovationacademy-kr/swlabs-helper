abd75b10123d9edf50275f830d423c0cb8f72f70
HyunJin Won <ohj8447@gmail.com>
2021-07-23 17:41:31 +0900

    수정: #297 ApiController try-catch AOP 설정
    추가: AOP 적용 기준인 ApiControllerTryCatch interface 추가
    추가: ApiController 의 try-catch 를 처리하는 AOP 클래스, 메소드 추가
    수정: ApiController 에 try-catch AOP 적용

--- src/main/java/io/seoul/helper/controller/user/UserApiController.java
+++ src/main/java/io/seoul/helper/controller/user/UserApiController.java
@@ -1,25 +1,25 @@
 package io.seoul.helper.controller.user;
 
+import io.seoul.helper.config.aop.ApiControllerTryCatch;
 import io.seoul.helper.config.auth.LoginUser;
 import io.seoul.helper.config.auth.dto.SessionUser;
 import io.seoul.helper.controller.dto.ResultResponseDto;
 import io.seoul.helper.controller.user.dto.UserResponseDto;
+import io.seoul.helper.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class UserApiController {
+    private final UserService userService;
 
     @GetMapping(value = "/api/v1/user/current")
-    public ResultResponseDto getCurrentUser(@LoginUser SessionUser sessionUser) {
-        UserResponseDto user = null;
-        if (sessionUser != null)
-            user = UserResponseDto.builder()
-                    .nickname(sessionUser.getNickname())
-                    .name(sessionUser.getNickname())
-                    .email(sessionUser.getEmail())
-                    .build();
+    @ApiControllerTryCatch
+    public ResultResponseDto getCurrentUser(@LoginUser SessionUser sessionUser) throws Throwable {
+        UserResponseDto user = userService.findUserBySession(sessionUser);
         return ResultResponseDto.builder()
                 .statusCode(HttpStatus.OK.value())
                 .message(HttpStatus.OK.name())
