da89561a909242beaf3e9264d2b144ddcfe15856
HyunJin Won <ohj8447@gmail.com>
2021-10-09 22:15:36 +0900

    edit #349 : 일부 페이지 변경
    delete : admin/home 제거
    edit: header_admin의 버튼 활성화 버그 수정, settle, settle_history의 currentPage 변수 내역 미수정이 원인

--- src/main/java/io/seoul/helper/controller/PageAdminController.java
+++ src/main/java/io/seoul/helper/controller/PageAdminController.java
@@ -4,8 +4,6 @@ import io.seoul.helper.config.auth.LoginUser;
 import io.seoul.helper.config.auth.dto.SessionUser;
 import io.seoul.helper.controller.review.dto.ReviewNeedSettleResponseDto;
 import io.seoul.helper.controller.settle.dto.SettleNeedPaidGroupByUserResponseDto;
-import io.seoul.helper.controller.team.dto.TeamCountRequestDto;
-import io.seoul.helper.controller.team.dto.TeamCountResponseDto;
 import io.seoul.helper.service.ReviewService;
 import io.seoul.helper.service.SettleService;
 import io.seoul.helper.service.TeamService;
@@ -17,9 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAdjusters;
 import java.util.List;
 
 @Slf4j
@@ -30,27 +25,6 @@ public class PageAdminController {
     private final TeamService teamService;
     private final ReviewService reviewService;
     private final SettleService settleService;
-
-    @GetMapping("index")
-    public String home(Model model, @LoginUser SessionUser user) {
-        if (user == null)
-            return "index";
-
-        LocalDateTime start = LocalDateTime.now().with(TemporalAdjusters.firstDayOfMonth())
-                .withHour(0).withMinute(0).withSecond(0).withNano(0);
-        LocalDateTime end = LocalDateTime.now().plusMonths(1).with(TemporalAdjusters.firstDayOfMonth())
-                .withHour(0).withMinute(0).withSecond(0).withNano(0);
-        TeamCountRequestDto teamCountRequestDto = TeamCountRequestDto.builder()
-                .start(start)
-                .end(end)
-                .build();
-
-        TeamCountResponseDto responseDto = teamService.getTeamCount(teamCountRequestDto);
-        model.addAttribute("user", user);
-        model.addAttribute("teamCount", responseDto);
-        model.addAttribute("today", start.format(DateTimeFormatter.ofPattern("yyyy.MM")));
-        return "admin/index";
-    }
 
     @GetMapping("settle")
     public String settle(Model model, @LoginUser SessionUser user, @RequestParam(defaultValue = "10") int limit) {
